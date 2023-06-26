423c2550714f868b2b0ab951f5462191eadaccf4
coderhzy <1257664828@qq.com>
2023-06-26 17:33:15 +0800

    feat(封装请求返回拦截器): 封装请求返回拦截器

--- apps/low-code-back/src/app.controller.ts
+++ apps/low-code-back/src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Version } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'user',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Version('2')
+  @Version([VERSION_NEUTRAL, '1'])
   // http://localhost:3000/v2
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Version('2')
+  findUser(): string {
+    return 'findUser';
   }
 }
--- apps/low-code-back/src/main.ts
+++ apps/low-code-back/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from '../../../libs/comm/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 接口版本化管理
   app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
+
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
 }
